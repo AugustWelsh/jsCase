77201f80b4bd1b426c1084c29273fcc6696b4e55
AugustWelsh <90603561+AugustWelsh@users.noreply.github.com>
2021-11-30 23:04:34 +0100

    Add files via upload

--- jsCase.js
+++ jsCase.js
@@ -1,3 +1,77 @@
-function todoList() {
-    //store user value
-}
+// todoList function used on button click to get user input and 
+//function todoList() {
+	// store user value
+	//var item    = document.getElementById('todoInput').value
+	// create a text node from the user input
+	//var text    = document.createTextNode(item)
+	// create a li tag
+	//var newItem = document.createElement('li')
+	// add the user input to the li tag
+	//newItem.appendChild(text)
+	// append the li to the html todoList id tag
+	//document.getElementById('todoList').appendChild(newItem)
+
+   // text.addEventListener('click', function(){
+        
+  // })
+
+//}
+
+// ---------------------------------------------
+
+//
+//let inputValu = document.querySelector('input');
+//let btn = document.getElementsByTagName('button');
+//let ulList = document.getElementById('myUl');
+//let doneList = document.getElementById('done')
+//
+////btn[0].onclick = function () {
+//let createLi = document.createElement('li')
+//createLi.innerHTML = inputValu.value;
+//ulList.appendChild(createLi);
+////
+//createLi.onclick = function () {
+//createLi.style.textDecoration = 'line-through';
+//doneList.appendChild(createLi)
+//////}
+////doneList.ondblclick = function () {
+//doneList.removeChild(createLi);
+//}
+//}//
+
+//Create a new list item – var li = document.createElement("li"); li.innerHTML = "Item";
+//Append it to the list – document.getElementById("LIST").appendChild(li);
+//To remove a list item in Javascript:
+
+//Get all the list items – var all = document.querySelectorAll("#LIST li");
+//Remove the corresponding item – document.getElementById("LIST").removeChild(all[0]);
+
+
+//function remove(el) {
+  //  var element = el;
+  //  element.remove();
+ // }
+
+ //https://pretagteam.com/question/javascript-need-to-remove-li-element-when-clicked
+
+ //https://morioh.com/p/a6498307a7ba
+
+ //https://javascript.info/introduction-browser-events
+
+ let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
