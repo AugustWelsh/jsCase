90a89cc7a06e4cac3921aacdd90d30e78b5d27f6
AugustWelsh <90603561+AugustWelsh@users.noreply.github.com>
2021-12-01 10:39:26 +0100

    Add files via upload

--- jsCase.js
+++ jsCase.js
@@ -58,20 +58,20 @@
 
  //https://javascript.info/introduction-browser-events
 
- let addToDoButton = document.getElementById('addToDo');
-let toDoContainer = document.getElementById('toDoContainer');
-let inputField = document.getElementById('inputField');
+ //let addToDoButton = document.getElementById('addList');
+//let toDoContainer = document.getElementById('toDoListContainer');
+//let inputField = document.getElementById('inputArea');
 
-addToDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = "";
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-    paragraph.addEventListener('dblclick', function(){
-        toDoContainer.removeChild(paragraph);
-    })
-})
+//addToDoButton.addEventListener('click', function(){
+    //var paragraph = document.createElement('p');
+    //paragraph.classList.add('paragraph-styling');
+    //paragraph.innerText = inputField.value;
+    //toDoContainer.appendChild(paragraph);
+    //inputField.value = "";
+   // paragraph.addEventListener('click', function(){
+   //     paragraph.style.textDecoration = "line-through";
+   // })
+    //paragraph.addEventListener('dblclick', function(){
+   //     toDoContainer.removeChild(paragraph);
+   // })
+//})
