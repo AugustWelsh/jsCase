43f52e62abb0cf3e54563c39d4e4890e86c1e824
AugustWelsh <90603561+AugustWelsh@users.noreply.github.com>
2021-12-08 12:50:51 +0100

    Add files via upload

--- jsCase.js
+++ jsCase.js
@@ -1,99 +1,43 @@
-// todoList function used on button click to get user input and 
-//function todoList() {
-	// store user value
-	//var item    = document.getElementById('todoInput').value
-	// create a text node from the user input
-	//var text    = document.createTextNode(item)
-	// create a li tag
-	//var newItem = document.createElement('li')
-	// add the user input to the li tag
-	//newItem.appendChild(text)
-	// append the li to the html todoList id tag
-	//document.getElementById('todoList').appendChild(newItem)
-
-   // text.addEventListener('click', function(){
-        
-  // })
-
-//}
-
-// ---------------------------------------------
-
-//
-//let inputValu = document.querySelector('input');
-//let btn = document.getElementsByTagName('button');
-//let ulList = document.getElementById('myUl');
-//let doneList = document.getElementById('done')
-//
-////btn[0].onclick = function () {
-//let createLi = document.createElement('li')
-//createLi.innerHTML = inputValu.value;
-//ulList.appendChild(createLi);
-////
-//createLi.onclick = function () {
-//createLi.style.textDecoration = 'line-through';
-//doneList.appendChild(createLi)
-//////}
-////doneList.ondblclick = function () {
-//doneList.removeChild(createLi);
-//}
-//}//
-
-//Create a new list item – var li = document.createElement("li"); li.innerHTML = "Item";
-//Append it to the list – document.getElementById("LIST").appendChild(li);
-//To remove a list item in Javascript:
-
-//Get all the list items – var all = document.querySelectorAll("#LIST li");
-//Remove the corresponding item – document.getElementById("LIST").removeChild(all[0]);
-
-
-//function remove(el) {
-  //  var element = el;
-  //  element.remove();
- // }
-
- //https://pretagteam.com/question/javascript-need-to-remove-li-element-when-clicked
-
- //https://morioh.com/p/a6498307a7ba
-
- //https://javascript.info/introduction-browser-events
-
- //let addToDoButton = document.getElementById('addList');
-//let toDoContainer = document.getElementById('toDoListContainer');
-//let inputField = document.getElementById('inputArea');
-
-
-
-//https://www.geeksforgeeks.org/how-to-create-todo-app-using-html-css-js-and-bootstrap/
-
-function playMyAudio(){
-  document.getElementById("myAudio").play();
-}
+//hämta info från html
 var addToDoButton = document.getElementById('addToDo');
+var clearToDo = document.getElementById('clearToDo')
 
+//spela ljud när man lägger till en task
+var x = document.getElementById("myAudio"); 
 
+function playAudio() { 
+  x.play(); 
+} 
+//clear button
+function removeAll(){
+  document.getElementById("toDoContainer").innerHTML = "";
+}
 
+//lägga till click funktion, så att när jag clickar så skapas en ny li
 addToDoButton.addEventListener('click', function(){
-  var paragraph = document.createElement('p');
-
-
-  paragraph.classList.add('paragraph-styling');
- 
-  paragraph.innerText = inputField.value;
-  toDoContainer.appendChild(paragraph);
-  inputField.value = "";
-  paragraph.addEventListener('click', function(){
-      paragraph.style.textDecoration = "underline";
-  })
-  paragraph.addEventListener('click', function(){
-    paragraph.style.color = "Red";
-  })
+  var list = document.createElement('li');
   
-  paragraph.addEventListener('dblclick', function(){
-      toDoContainer.removeChild(paragraph);
+
+  //lägg till styling på p
+  list.classList.add('list-styling');
+  list.innerText = inputField.value;
+  toDoContainer.appendChild(list);
+  inputField.value = "";
+  list.addEventListener('click', function(){
+      list.style.textDecoration = "underline";
+      
+  })
+  //gör li-text röd genom att clicka på texten
+  list.addEventListener('click', function(){
+    list.style.color = "Red";
+    
+  })
+  //dubble clicka för att ta bort li
+  list.addEventListener('dblclick', function(){
+      toDoContainer.removeChild(list);
       
       
   })
-
+      
 
 })
